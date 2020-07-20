8ab46ae1de5bec109087df7e195566c3dd54f022
Ming <319407@qq.com>
2020-07-21 00:01:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,22 @@ Created on Mon May 13 10:49:16 2019
 """
 
 
-
 import os
 import sys
 pwd = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(pwd)
-
-
-
 import numpy as np
 from bayes import train,read_vector
 
 
 
-
 if __name__ =='__main__':
-    ### 训练 
-    #读取变量
+    # Load y
     labels = np.loadtxt(os.path.join(pwd,'data','types.txt'))
-    #读取词袋
+    # Load x
     #vectors = read_vector('vector_pearson_40000.txt')
     vectors = read_vector('vectors1000.txt')
-    #训练参数
+    # Training
     p0Vec,p1Vec,pClass1 = train(vectors,labels)
 
 
